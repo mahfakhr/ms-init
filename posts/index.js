bdff04e3053a435c4e8daf63128390d67d4febdf
Mahnaz Fakhr <mahfakhr2009@gmail.com>
2022-11-25 11:56:41 +0330

    bug fixed on posts api route

--- posts/index.js
+++ posts/index.js
@@ -4,11 +4,11 @@ const { randomBytes } = require('crypto');
 const app = express();
 app.use(bodyParser.json());
 const posts = {};
-app.get('/posts`', (req, res) => {
+app.get('/posts', (req, res) => {
 	res.send(posts);
 });
 
-app.post('/posts`', (req, res) => {
+app.post('/posts', (req, res) => {
 	const id = randomBytes(4).toString('hex');
 	const { title } = req.body;
 	posts[id] = { id, title };
